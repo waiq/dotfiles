723c524c5593cf5b3b84e91c376da8fc9e106773
dotfiles <>
2024-10-18 17:15:35 +0200

    add wezterm

---
+++ custom/roles/wezterm/files/wezterm.lua
@@ -0,0 +1,14 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Setup color_scheme:
+config.color_scheme = "Catppuccin Mocha"
+
+config.enable_tab_bar = false
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+
+return config
