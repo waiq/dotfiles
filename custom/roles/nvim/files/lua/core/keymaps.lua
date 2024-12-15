183a8c799b205ec66d04c3cde1eebac66664ce5a
dotfiles <>
2024-12-15 14:50:19 +0100

    wip

--- custom/roles/nvim/files/lua/core/keymaps.lua
+++ custom/roles/nvim/files/lua/core/keymaps.lua
@@ -73,3 +73,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Oil
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent foler' })
