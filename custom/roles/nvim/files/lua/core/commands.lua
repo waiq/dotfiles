6baaaaa3f981a8fef00c5c864d1e89c7b2e695b3
dotfiles <>
2024-12-15 13:46:09 +0100

    wip

---
+++ custom/roles/nvim/files/lua/core/commands.lua
@@ -0,0 +1,3 @@
+-- more stuff
+vim.api.nvim_create_user_command('Wa', 'wa', {})
+vim.api.nvim_create_user_command('Qa', 'qa', {})
---
+++ custom/roles/nvim/files/lua/plugins/dap.lua
@@ -0,0 +1,12 @@
+return {
+  'jay-babu/mason-nvim-dap.nvim',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'mfussenegger/nvim-dap',
+  },
+  config = function()
+    require('mason-nvim-dap').setup {
+      ensure_installed = { 'python', 'delve' },
+    }
+  end,
+}
