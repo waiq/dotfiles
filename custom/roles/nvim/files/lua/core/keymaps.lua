a5b0320968677cbe5793ed32e40ba2d4e04dfa5e
dotfiles <>
2024-12-17 10:55:49 +0100

    wip

--- custom/roles/nvim/files/lua/core/keymaps.lua
+++ custom/roles/nvim/files/lua/core/keymaps.lua
@@ -14,9 +14,6 @@ vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
 -- save file without auto-formatting
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
 
--- quit file
-vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
-
 -- delete single character without copying into register
 vim.keymap.set('n', 'x', '"_x', opts)
 
