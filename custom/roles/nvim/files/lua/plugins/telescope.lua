6e3b4feebdbec3729aaa9045fa4ec904faa89d0d
dotfiles <>
2025-01-24 09:48:55 +0100

    nvim change selector

--- custom/roles/nvim/files/lua/plugins/telescope.lua
+++ custom/roles/nvim/files/lua/plugins/telescope.lua
@@ -52,7 +52,7 @@ return {
           i = {
             ['<C-k>'] = require('telescope.actions').move_selection_previous, -- move to prev result
             ['<C-j>'] = require('telescope.actions').move_selection_next, -- move to next result
-            ['<C-l>'] = require('telescope.actions').select_default, -- open file
+            ['<C-y>'] = require('telescope.actions').select_default, -- open file
           },
         },
       },
